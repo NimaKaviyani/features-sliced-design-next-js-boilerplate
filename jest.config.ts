bf3931fc76fc906ff7e61eee586bd63fa19de740
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-09-03 15:52:06 +0330

    style: format jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -33,7 +33,7 @@ const config: Config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: "v8"
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
