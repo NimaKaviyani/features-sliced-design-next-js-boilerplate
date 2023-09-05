c1cbdebf3de0d58e9150338bba5536f8336fbef7
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-09-05 11:19:24 +0330

    feat: change api structure

--- app/shared/api/eoap/index.ts
+++ app/shared/api/eoap/index.ts
@@ -1,1 +1,29 @@
-export * from "./base";
+import { AxiosResponse } from "axios";
+import api from "@shared/api/eoap/base";
+import {
+  DeleteRequestFunction,
+  GetRequestFunction,
+  PostRequestFunction,
+  PutRequestFunction,
+  ResponseData
+} from "@shared/api/eoap/models";
+
+export const getRequest: GetRequestFunction = async (url, params, authorization): Promise<ResponseData> => {
+  const response: AxiosResponse<ResponseData> = await api(authorization).get(url, { params });
+  return response?.data;
+};
+
+export const postRequest: PostRequestFunction = async (url, data, authorization): Promise<ResponseData> => {
+  const response: AxiosResponse<ResponseData> = await api(authorization).post(url, data);
+  return response?.data;
+};
+
+export const putRequest: PutRequestFunction = async (url, data, authorization): Promise<ResponseData> => {
+  const response: AxiosResponse<ResponseData> = await api(authorization).put(url, data);
+  return response?.data;
+};
+
+export const deleteRequest: DeleteRequestFunction = async (url, params, authorization): Promise<ResponseData> => {
+  const response: AxiosResponse<ResponseData> = await api(authorization).delete(url, { params });
+  return response?.data;
+};
--- app/shared/api/models.ts
+++
@@ -1,1 +0,0 @@
-export * from "./eoap/models";
