778d071f85f247a1ebb39a03d6fa99841381b4d3
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-09-05 11:30:50 +0330

    feat: add panel level

---
+++ app/(pages)/panel/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const Dashboard = (): ReactNode => {
+  return <></>;
+};
+
+export default Dashboard;
---
+++ app/(pages)/panel/layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const Layout = (): ReactNode => {
+  return <></>;
+};
+
+export default Layout;
