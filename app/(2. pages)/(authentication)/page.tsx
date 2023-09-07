acd6a6534d32427573bbfb955e32eb867b687a3b
mohammad-davari-82 <mohammaddavarzani1998@gmail.com>
2023-09-07 22:05:59 +0430

    refactor: change folder structure

--- app/(2. pages)/(authentication)/page.tsx
+++ app/(2. pages)/(authentication)/page.tsx
@@ -2,7 +2,7 @@
 import { ReactNode } from "react";
 import { Center, Box } from "@mantine/core";
 import { loginLanguage } from "@shared/i18n/locales/fa";
-import LoginForm from "@widgets/login/login-form";
+import LoginForm from "@/3. widgets/login-form/ui";
 
 export default function Login(): ReactNode {
   return (
