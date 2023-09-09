208e3bb886edfe4868eead79d10ede3f271fa210
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-09-09 10:21:20 +0330

    Revert "refactor: change folder structure"
    This reverts commit acd6a6534d32427573bbfb955e32eb867b687a3b.

--- app/(2. pages)/(authentication)/page.tsx
+++ app/(2. pages)/(authentication)/page.tsx
@@ -2,7 +2,7 @@
 import { ReactNode } from "react";
 import { Center, Box } from "@mantine/core";
 import { loginLanguage } from "@shared/i18n/locales/fa";
-import LoginForm from "@/3. widgets/login-form/ui";
+import LoginForm from "@widgets/login/login-form";
 
 export default function Login(): ReactNode {
   return (
