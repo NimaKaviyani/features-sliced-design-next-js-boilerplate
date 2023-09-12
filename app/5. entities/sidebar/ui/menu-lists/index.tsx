bc287e121224ec68b238f4776358d365dfc466e7
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-09-12 14:51:20 +0330

    refactor: cleanup

--- app/5. entities/sidebar/ui/menu-lists/index.tsx
+++ app/5. entities/sidebar/ui/menu-lists/index.tsx
@@ -6,52 +6,50 @@ import {
   IconPresentationAnalytics,
   IconFileAnalytics,
   IconAdjustments,
-  IconLock,
-} from '@tabler/icons-react';
-import {LinksGroup}  from "@shared/ui/menu";
+  IconLock
+} from "@tabler/icons-react";
+import { LinksGroup } from "@shared/ui/menu";
 
 /* todo: change with real data */
 const mockdata = [
-  { label: 'Dashboard', icon: IconGauge },
+  { label: "Dashboard", icon: IconGauge },
   {
-    label: 'Market news',
+    label: "Market news",
     icon: IconNotes,
     initiallyOpened: true,
     links: [
-      { label: 'Overview', link: '/' },
-      { label: 'Forecasts', link: '/' },
-      { label: 'Outlook', link: '/' },
-      { label: 'Real time', link: '/' },
-    ],
+      { label: "Overview", link: "/" },
+      { label: "Forecasts", link: "/" },
+      { label: "Outlook", link: "/" },
+      { label: "Real time", link: "/" }
+    ]
   },
   {
-    label: 'Releases',
+    label: "Releases",
     icon: IconCalendarStats,
     links: [
-      { label: 'Upcoming releases', link: '/' },
-      { label: 'Previous releases', link: '/' },
-      { label: 'Releases schedule', link: '/' },
-    ],
+      { label: "Upcoming releases", link: "/" },
+      { label: "Previous releases", link: "/" },
+      { label: "Releases schedule", link: "/" }
+    ]
   },
-  { label: 'Analytics', icon: IconPresentationAnalytics },
-  { label: 'Contracts', icon: IconFileAnalytics },
-  { label: 'Settings', icon: IconAdjustments },
+  { label: "Analytics", icon: IconPresentationAnalytics },
+  { label: "Contracts", icon: IconFileAnalytics },
+  { label: "Settings", icon: IconAdjustments },
   {
-    label: 'Security',
+    label: "Security",
     icon: IconLock,
     links: [
-      { label: 'Enable 2FA', link: '/' },
-      { label: 'Change password', link: '/' },
-      { label: 'Recovery codes', link: '/' },
-    ],
-  },
+      { label: "Enable 2FA", link: "/" },
+      { label: "Change password", link: "/" },
+      { label: "Recovery codes", link: "/" }
+    ]
+  }
 ];
 export default function MenuLists(): ReactNode {
-  const links = mockdata.map((item) => <LinksGroup icon={item.icon} label={item.label} links={item.links} key={item.label} />);
+  const links = mockdata.map(item => (
+    <LinksGroup icon={item.icon} label={item.label} links={item.links} key={item.label} />
+  ));
 
-  return (
-    <div>
-      {links}
-    </div>
-  );
+  return <div>{links}</div>;
 }
