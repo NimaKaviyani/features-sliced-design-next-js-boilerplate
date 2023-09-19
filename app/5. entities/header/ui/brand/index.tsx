a49327aabbbb39baa01b74261a0ca064d222997a
mohammadreza-davarzani-82 <74320753+mohammadreza-davarzani-82@users.noreply.github.com>
2023-09-19 08:28:11 +0330

    fix: remove padding for align item in brand

--- app/5. entities/header/ui/brand/index.tsx
+++ app/5. entities/header/ui/brand/index.tsx
@@ -1,16 +1,10 @@
 import React, { ReactNode } from "react";
-import { Box, CSSObject, Group, MantineTheme } from "@mantine/core";
+import { Box,Group} from "@mantine/core";
 import Logo from "@shared/ui/logo";
 
 const Brand = (): ReactNode => {
   return (
-    <Box
-      sx={(theme: MantineTheme): CSSObject => ({
-        paddingLeft: theme.spacing.xs,
-        paddingRight: theme.spacing.xs,
-        paddingBottom: theme.spacing.lg
-      })}
-    >
+    <Box>
       <Group position="apart">
         <Logo />
       </Group>
