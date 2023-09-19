bea13e18c9bac796862996d393f49dc52e5acda6
mohammadreza-davarzani-82 <74320753+mohammadreza-davarzani-82@users.noreply.github.com>
2023-09-19 11:01:57 +0330

    feat: adding real data to side-bar & create routing for sms

--- app/5. entities/sidebar/ui/menu-lists/index.tsx
+++ app/5. entities/sidebar/ui/menu-lists/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { IconUsersPlus, IconLayoutDashboard } from "@tabler/icons-react";
+import { IconUsersPlus, IconLayoutDashboard,IconMail} from "@tabler/icons-react";
 import { LinksGroup } from "@shared/ui/menu";
 
 /* todo: change with real data */
@@ -9,7 +9,13 @@ const mockdata = [
     label: "مشترکین",
     icon: IconUsersPlus,
     initiallyOpened: true,
-    subMenus: [{ label: "مدیریت مشترکین", link: "/panel/subscriber" }]
+    subMenus: [{ label: "مدیریت مشترکین", link: "/panel/subscriber/subscriber-management" }]
+  },
+  {
+    label: "پیامک",
+    icon: IconMail,
+    initiallyOpened: true,
+    subMenus: [{ label: "صندوق ارسال پیامک تک", link: "/panel/sms/single-sms" },{ label: "صندوق ارسال پیامک انبوه", link: "/panel/sms/bulk-sms" }]
   }
 ];
 export default function MenuLists(): ReactNode {
