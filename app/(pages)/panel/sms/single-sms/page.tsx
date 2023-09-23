947c182fbae2898b56086dc3038de4b9eaa93984
mohammadreza-davarzani-82 <74320753+mohammadreza-davarzani-82@users.noreply.github.com>
2023-09-23 21:29:23 +0330

    feat: adding container for page & layout

--- app/(pages)/panel/sms/single-sms/page.tsx
+++ app/(pages)/panel/sms/single-sms/page.tsx
@@ -1,7 +1,25 @@
+"use client";
 import React, { ReactNode } from "react";
+import { Grid, Divider } from "@mantine/core";
+import GetUserInformation from "@/features/get-user-information";
+import UserInformation from "@/widgets/panel/ui/user-information";
 
 const SingleSms = (): ReactNode => {
-  return <>single-sms</>;
+  return (
+    <Grid>
+      <Grid.Col>
+        <h1>صندوق ارسال پیامک تک</h1>
+      <Divider my="sm" />
+      </Grid.Col>
+      <Grid.Col>
+        <GetUserInformation />
+      </Grid.Col>
+      <Grid.Col>
+        <UserInformation />
+      </Grid.Col>
+    </Grid>
+  );
 };
 
 export default SingleSms;
+
