c50c2c2112d994b39f3e1af1b3786968dbc0533e
mohammadreza-davarzani-82 <74320753+mohammadreza-davarzani-82@users.noreply.github.com>
2023-09-23 21:29:42 +0330

    feat: adding lang file for sms

--- app/shared/i18n/locales/fa/index.ts
+++ app/shared/i18n/locales/fa/index.ts
@@ -1,3 +1,5 @@
 export * from "./global";
 export * from "./auth";
 export * from "./panel/dashboard";
+export * from "./panel/sms";
+
---
+++ app/shared/i18n/locales/fa/panel/sms.ts
@@ -0,0 +1,21 @@
+export const smsLanguage = {
+  singleSms: {
+    Id: "شناسه",
+    search:"جستجو",
+    idValidate:"لطفا شناسه خود را انتخاب نمایید!",
+    phoneNumber: "شماره گیرنده",
+    phoneNumberValidate: "لطفا شماره خود را به صورت صحیح وارد نمایید!",
+    phoneNumberEmpty: "لطفا شماره خود را وارد نمایید!"
+
+  },
+  userInfo:{
+    customer:"مشتری",
+    idContact:"کد مخاطب",
+    smsIdLabel:"عنوان شناسه پیامک",
+    nameContact:"نام مخاطب",
+    sendPriority:"الویت ارسال",
+    inquiryState:"وضعیت استعلام",
+    providerName:"نام پروایدر",
+    sendingChannel:"کانال ارسال"
+  }
+};
