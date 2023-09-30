ffaf919b00346dbc918cbccde3f8e103b8aa13cc
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-09-30 10:52:45 +0330

    fix: error of text sms

--- app/shared/state-management/single-sms.ts
+++ app/shared/state-management/single-sms.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
+
 interface Tags {
   tags: string[];
   addTag: (tag: string) => void;
   removeTag: (tag: string) => void;
 }
+
 export const useTag = create<Tags>(set => ({
   tags: [],
   addTag: (tag: string): void =>
@@ -18,27 +20,33 @@ export const useTag = create<Tags>(set => ({
     }),
   removeTag: (tag): void => set(prevState => ({ tags: prevState.tags.filter(item => item !== tag) }))
 }));
+
 interface useGetInformation {
-  phoneNumber: string,
-  id: string,
-  
-  getUser: (tag: string , id: string ) => void;
+  phoneNumber: string;
+  id: string;
+
+  getUser: (tag: string, id: string) => void;
 }
+
 export const useGetInformation = create<useGetInformation>(set => ({
   phoneNumber: "",
-  id:"",
-  getUser: (number: string , id: string ): void => set({phoneNumber:number,id:id}),
+  id: "",
+  getUser: (number: string, id: string): void => set({ phoneNumber: number, id: id })
 }));
+
 interface UseTextSMS {
-  textSMS: {value?:string, error?: boolean, message?: string}
-  setTextSMS: ({value, error, message}:{value?: string , error?: boolean, message?: string} ) => void;
+  textSMS: { value?: string; error?: boolean; message?: string };
+  setTextSMS: ({ value, error, message }: { value?: string; error?: boolean; message?: string }) => void;
 }
+
 export const useTextSMS = create<UseTextSMS>(set => ({
-  textSMS: {value:"", error: false, message: ""}
-  setTextSMS: ({value , error, message}) => set({textSMS:{
-        ...(value && {value}),
-        ...(error && {error}),
-        ...(message && {message}),
+  textSMS: { value: "", error: false, message: "" },
+  setTextSMS: ({ value, error, message }): void =>
+    set({
+      textSMS: {
+        ...(value && { value }),
+        ...(error && { error }),
+        ...(message && { message })
       }
     })
 }));
